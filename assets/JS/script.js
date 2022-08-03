22fe5e9e723b37a8a6582cb34de3a54b23dbf55e
Mykindofgeeky <103599638+georgeneves@users.noreply.github.com>
2022-08-03 10:05:54 -0500

    js questions and answers set up pt 1

---
+++ assets/JS/script.js
@@ -0,0 +1,60 @@
+var startBtn = document.querySelector('.start');
+var question = document.querySelector('.question');
+var answer1 = document.querySelector('.answer1');
+var answer2 = document.querySelector('.answer2');
+var answer3 = document.querySelector('.answer3');
+var answer4 = document.querySelector('.answer4');
+var answer5 = document.querySelector('.answer5');
+var questions = [
+    { number:1,
+    question: 'JavaScript is a ___-side programming language.',
+    choices: ['shore', ' clients', 'server', 'dark'],
+    answer: 'client',
+    },
+    { number:2,
+    question: 'The condition in an it / else statement is enclosed within ___.',
+    choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+    answer: 'parentheses',
+    },
+    {number:3,
+    question: 'Arrays in JavaScript can be used to store ___.',
+    choices ['numbers and strings', 'other arrays', 'booleans', 'all of the above',],
+    answer: 'all of the above'}
+]
+
+document.body.className
+
+function start() {
+    //set up
+}
+
+
+function question1() {
+    //display question1
+}
+
+function question2() {
+    //display question2
+}
+
+function question3() {
+    //display question3
+}
+
+function question4() {
+    //display question4
+}
+
+function question5() {
+    //display question5
+}
+
+function hideQuestion() {
+    //hide question
+}
+
+function checkAnswer() {
+    //checkAnswer
+}
+
+addEventListener()
